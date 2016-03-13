bf589e6b0f54d734f12aeeae3ea8e84f1905584a
Lucas <lucas@agtlucas.com>
2016-03-13 14:24:29 -0300

    Update wepback

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+'use strict'
+
+var path = require('path')
+var webpack = require('webpack')
+var autoprefixer = require('autoprefixer')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  devtool: 'eval-source-map',
+
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, 'src/js/app.js')
+  ],
+
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].min.js',
+    publicPath: '/'
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    new ExtractTextPlugin('style.css', {
+      allChunks: true
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }, {
+      test: /\.(styl|css)$/,
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader')
+    }],
+
+    resolve: {
+      extensions: ['', '.js', '.styl', '.css']
+    }
+  },
+
+  postcss: function () {
+    return [autoprefixer]
+  }
+}
