008703102055dea59a7ba92342840dc34578b6d7
Lucas <lucas@agtlucas.com>
2016-03-13 15:49:38 -0300

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
       loader: 'babel'
     }, {
       test: /\.(styl|css)$/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader')
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader!stylus-loader')
     }],
 
     resolve: {
